300e9ef5e99cc0d57ece48c1c38eb5a234fc4aa1
niebing <62319054+Mxd353@users.noreply.github.com>
2022-05-11 00:39:47 +0800

    Add files via upload

---
+++ ball.cpp
@@ -0,0 +1,53 @@
+#include "ball.h"
+
+#include <QGraphicsScene>
+#include <QPainter>
+#include <QRandomGenerator>
+#include <QStyleOption>
+#include <QtMath>
+#include <QGraphicsRectItem>
+#include <QDebug>
+
+Ball::Ball()
+{}
+
+QRectF Ball::boundingRect() const
+{
+    qreal penWidth=1;
+    return QRectF(po_x - penWidth / 2, po_y - penWidth / 2,
+                  radius + penWidth, radius + penWidth);
+}
+
+QPainterPath Ball::shape() const
+{
+    QPainterPath path;
+    path.addRect(po_x, po_y, radius, radius);
+    return path;
+}
+
+void Ball::paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidget *)
+{
+    painter->setBrush(scene()->collidingItems(this).isEmpty() ? Qt::red : Qt::green);//碰撞检测
+    painter->drawEllipse(po_x, po_y, radius, radius);
+}
+//! [3]
+
+//! [4]
+void Ball::advance(int step)
+{
+    if(!step)
+        return;
+
+       if(!collidingItems().isEmpty()) //碰撞检测
+        {
+           if(x() <= -330  || x() >= 330)xspeed*=-1;
+           if(y() <= -330  || y() >= 330)yspeed*=-1;
+        }
+            moveBy(xspeed, yspeed);
+}
+
+void Ball::setSpeed(double Xspeed, double Yspeed)
+{
+    xspeed = Xspeed;
+    yspeed = Yspeed;
+}
---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <QtMath>
+#include <QtWidgets>
+
+#include "ball.h"
+
+
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+
+    Ball *ball = new Ball;
+
+    QGraphicsLineItem *line1 = new QGraphicsLineItem(0, 0, 0, 700);
+    QGraphicsLineItem *line2 = new QGraphicsLineItem(0, 700, 700, 700);
+    QGraphicsLineItem *line3 = new QGraphicsLineItem(700, 700, 700, 0);
+    QGraphicsLineItem *line4 = new QGraphicsLineItem(700, 0, 0, 0);
+
+    QGraphicsScene scene;
+        scene.setItemIndexMethod(QGraphicsScene::NoIndex);
+        scene.setSceneRect(0, 0, 700, 700);
+
+    QGraphicsView view(&scene);
+        view.setRenderHint(QPainter::Antialiasing);
+        view.setCacheMode(QGraphicsView::CacheBackground);
+        view.setViewportUpdateMode(QGraphicsView::FullViewportUpdate );
+        view.resize(750, 750);
+
+        scene.addItem(line1);
+        scene.addItem(line2);
+        scene.addItem(line3);
+        scene.addItem(line4);
+
+        scene.addItem(ball);
+
+        //随机方向
+        qreal x = QRandomGenerator :: global() -> bounded( -300 , 300 );
+        qreal y = QRandomGenerator :: global() -> bounded( -300 , 300 );
+        //随机速度
+        double xSpeed = QRandomGenerator :: global() -> bounded( -10 , 10 );
+        double ySpeed = QRandomGenerator :: global() -> bounded( -10 , 10 );
+
+        ball->setPos(x,y);
+        ball->setSpeed(xSpeed,ySpeed);
+
+        view.setWindowTitle("随机方向,随机速度");
+        view.show();
+
+
+    QTimer timer;
+    QObject::connect(&timer, &QTimer::timeout, &scene, &QGraphicsScene::advance);
+    timer.start(1000/33);
+
+    return app.exec();
+}
